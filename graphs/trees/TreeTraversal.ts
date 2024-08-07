ab2334c7df3a9b9cae4a06d5969bc6a0a5a65330
Kayla Killian <killiankayla.e@gmail.conm>
2024-08-07 17:30:02 -0400

    added preOrderTrav iterative

--- graphs/trees/TreeTraversal.ts
+++ graphs/trees/TreeTraversal.ts
@@ -1,8 +1,11 @@
+import { TreeNodeImp } from "../../data_structures/TreeNode";
+import { Stack } from "../../data_structures/Stack";
+
 interface Traversal {
-    preOrderTrav(node: TreeNode): void;
-    inOrderTrav(node: TreeNode): void;
-    postOrderTrav(node: TreeNode): void;
-    visit(node: TreeNode): void;
+    preOrderTrav(node: TreeNodeImp): void;
+    inOrderTrav(node: TreeNodeImp): void;
+    postOrderTrav(node: TreeNodeImp): void;
+    visit(node: TreeNodeImp): void;
 }
 
 class TreeTraversal implements Traversal {
@@ -11,23 +14,45 @@ class TreeTraversal implements Traversal {
      * Visits all left children before the right.
      * @param node contains value and left/right pointers.
      */
-    preOrderTrav(node: TreeNode | null): void {
-        if (node != null) {
-            this.visit(node);
-            this.preOrderTrav(node.left);
-            this.preOrderTrav(node.right);
+    // preOrderTrav(node: TreeNode | null): void {
+    //     if (node != null) {
+    //         this.visit(node);
+    //         this.preOrderTrav(node.left);
+    //         this.preOrderTrav(node.right);
+    //     }
+    // }
+    /**
+     * To traverse a tree iteratively we use a stack
+     * @param node 
+     */
+    preOrderTrav(node: TreeNodeImp): void {
+        
+        const stack: Stack<TreeNodeImp> = new Stack();
+
+        stack.push(node);
+        
+        while (stack.size() !== 0) {
+            const current: TreeNodeImp = node;
+
+            this.visit(current);
+            if (current.right !== null) {
+                stack.push(current.right);
+            }
+            if (current.left !== null) {
+                stack.push(current.left);
+            }
         }
     }
 
-    inOrderTrav(node: TreeNode): void {
+    inOrderTrav(node: TreeNodeImp): void {
         
     }
 
-    postOrderTrav(node: TreeNode): void {
+    postOrderTrav(node: TreeNodeImp): void {
         
     }
 
-    visit(node: TreeNode): void {
+    visit(node: TreeNodeImp): void {
         console.log(node.value);
     }
 }
